2ab245553dcae818b1b01b21599675282bf68f3d
dimasergeev02 <justbringit16@yandex.ru>
2020-10-25 00:38:37 +0300

    1

--- sum_before_even_and_after_odd.c
+++ sum_before_even_and_after_odd.c
@@ -1,12 +1,19 @@
 #include "sum_between_even_odd.h"
-#include "sum_before_and_after_odd.h"
+#include "sum_before_even_and_after_odd.h"
 int sum_before_even_and_after_odd(int A[], int b)
 {
     int summ = 0;
     int i;
     for (i = 0; i < b; i++)
     {
-        summ = summ + abs(A[i]);
+        if (A[i]>=0)
+        {
+            summ = summ +A[i];
+        }
+        else
+        {
+            summ = summ - A[i];
+        }
     }
     summ = summ - sum_between_even_odd(A, b);
     return summ;
